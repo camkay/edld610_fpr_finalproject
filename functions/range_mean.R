b2fbe720bbb848e0ced951f05138d851cf1a4423
Cameron Kay <ckay@uoregon.edu>
2019-06-03 10:59:20 -0700

    Added description of range_mean and realized the function is poorly named. :(

--- functions/range_mean.R
+++ functions/range_mean.R
@@ -2,10 +2,16 @@
 #### Custom function 3 (at least 2 required) ####
 #################################################
 
-range_mean <- function(x) {
+# Takes a free response character range (e.g., "9-10 weeks") and calculates the 
+# mean of the range. Returns a character by default. 
+
+
+range_mean <- function(x, as.character = TRUE) {
   # ensure what is passed to the function is a character
   if (!is.character(x)) {
     stop("x must be of type character.")
+  } else if (!is.logical(as.character)) {
+    stop("as.character must of type logical.")
   }
   
   # attempt to find a mean for only those with a hyphen in the response
@@ -16,10 +22,15 @@ range_mean <- function(x) {
     x <- strsplit(x, "-")[[1]]
     # convert the string to numeric and calculate the mean
     x <- mean(as.numeric(x))
-    # maintaining the original type, convert the result back to a character
-    as.character(x)
+    # maintains the original type if as.character = TRUE
+    if (as.character == TRUE) {
+      return(as.character(x))
+    }
+    x
   } else {
     # if no hyphen, return x
     x
   }
 }
+
+range_mean("10-12", as.character = 20)
