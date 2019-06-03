33836c09097509e3652d015ace4f50d377e85174
Cameron Kay <ckay@uoregon.edu>
2019-06-03 10:51:36 -0700

    Added a description of paste_msd.

--- functions/paste_msd.R
+++ functions/paste_msd.R
@@ -2,6 +2,10 @@
 #### Custom function 10 (at least 2 required) ###
 #################################################
 
+# A simple function that combines a mean and an sd, couching the sd in 
+# parantheses. For example, given a mean of 5.59 and an sd of 1.29 the output 
+# will be 5.59(1.29).
+
 paste_msd <- function(mean, sd) {
   # paste mean and sd together and surround sd with parantheses
   paste0(mean, "(", sd, ")")
